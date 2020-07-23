e253f400d5c64c32904d79f16c5d263665623d74
Fatih-Ceylan <fceylan001@gmail.com>
2020-07-23 16:03:54 +0300

    Project Started

---
+++ Vidly .NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vidly_.NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
