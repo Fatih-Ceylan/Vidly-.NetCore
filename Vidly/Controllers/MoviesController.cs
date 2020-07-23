fee00a78502137bb1ecd5a267082665adec646c1
Fatih-Ceylan <fceylan001@gmail.com>
2020-07-23 19:54:51 +0300

    code updated

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Vidly.Models;
+
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
@@ -14,6 +15,22 @@ namespace Vidly.Controllers
             //return Content("hello world");
             //return NotFound();
             return RedirectToAction("Privacy", "Home");
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id" + id);
+        }
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(string.Format("pageIndex={0} & sortBy={1}", pageIndex, sortBy));
         }
     }
 }
--- Vidly/Models/Movies.cs
+++ Vidly/Models/Movies.cs
@@ -2,7 +2,6 @@
 {
     public class Movies
     {
-        // create an item here
         public int Id { get; set; }
         public string Name { get; set; }
     }
