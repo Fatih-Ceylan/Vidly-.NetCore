7f0568c44f74847aa1f969b84b37e41bcfa61b2a
Fatih-Ceylan <fceylan001@gmail.com>
2020-07-24 23:25:40 +0300

    Update ErrorViewModel.cs

--- Vidly/Models/ErrorViewModel.cs
+++ Vidly/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace Vidly.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
