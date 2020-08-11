e3f1a9780722c5516af60454e43cb9774c6fd31d
Fatih-Ceylan <fceylan001@gmail.com>
2020-08-11 20:53:37 +0300

    CustomerViewChanged

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -2,51 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using Vidly.Models;
-using Vidly.ViewModels;
 namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
-        static List<Customer> customers = new List<Customer>()
-        {
-            new Customer
-            {
-               Name ="Fatih Ceylan",
-               Id= 1,
-            },
-            new Customer
-            {
-                Name="Mahmut Tuncer",
-                Id=2,
-            },
-            new Customer
-            {
-                Name="İsmail Türüt",
-                Id=3,
-            }
-        };
+        //static List<Customer> customers = new List<Customer>()
+        //{
+        //    new Customer
+        //    {
+        //       Name ="Fatih Ceylan",
+        //       Id= 1,
+        //    },
+        //    new Customer
+        //    {
+        //        Name="Mahmut Tuncer",
+        //        Id=2,
+        //    },
+        //    new Customer
+        //    {
+        //        Name="İsmail Türüt",
+        //        Id=3,
+        //    }
+        //};
 
 
-        [Route("~/Customers")]
-        public IActionResult Customers()
+        //[Route("~/Customers")]
+        //public IActionResult Customers()
+        //{
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Customers = customers
+        //    };
+        //    return View(viewModel);
+        //}
+
+        //public IActionResult Details(int? id)
+        //{
+
+        //    var customer = customers.FirstOrDefault(a => a.Id == id);
+        //    if (customer == null)
+        //    {
+        //        return NotFound("Error");
+        //    }
+        //    return View(customer);
+
+        //}
+
+        public ViewResult Index()
         {
-            var viewModel = new RandomMovieViewModel
-            {
-                Customers = customers
-            };
-            return View(viewModel);
+            var customers = GetCustomers();
+            return View(customers);
         }
-
-        public IActionResult Details(int? id)
+        public ActionResult Details(int id)
         {
-
-            var customer = customers.FirstOrDefault(a => a.Id == id);
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
             if (customer == null)
-            {
-                return NotFound("Error");
-            }
+                return NotFound();
             return View(customer);
-
+        }
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer{Id=1,Name="John Smith"},
+                new Customer{Id=2,Name="Mary Jane"}
+            };
         }
     }
 }
