f4b1e06bdb96fd0e6f61b1478e9c0021179dca12
Fatih-Ceylan <fceylan001@gmail.com>
2020-08-16 15:24:32 +0300

    Exercise 1 updates

---
+++ Vidly/Migrations/20200816110942_AddingNameMembershipType.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Migrations
+{
+    public partial class AddingNameMembershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name" ,
+                table: "MembershipType" ,
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name" ,
+                table: "MembershipType");
+        }
+    }
+}
