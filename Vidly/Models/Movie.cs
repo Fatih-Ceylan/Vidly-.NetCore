91b5118a52f49f4210d62db067fc2f9b9c0eda54
Fatih-Ceylan <fceylan001@gmail.com>
2020-08-18 22:38:50 +0300

    Update Movie.cs

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,9 +1,14 @@
-﻿namespace Vidly.Models
+﻿using System;
+
+namespace Vidly.Models
 
 {
     public class Movie
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public DateTime? DateAdded { get; set; }
     }
 }
