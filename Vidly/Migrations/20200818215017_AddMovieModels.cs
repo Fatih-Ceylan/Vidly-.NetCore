8b8470fc39b3f102e3af91228ebe9d70d5220d1a
Fatih-Ceylan <fceylan001@gmail.com>
2020-08-19 01:44:37 +0300

    Checkpoint

---
+++ Vidly/Migrations/20200818215017_AddMovieModels.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace Vidly.Migrations
+{
+    public partial class AddMovieModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAdded" ,
+                table: "Movies" ,
+                type: "Date" ,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre" ,
+                table: "Movies" ,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate" ,
+                table: "Movies" ,
+                type: "Date" ,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateAdded" ,
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Genre" ,
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate" ,
+                table: "Movies");
+        }
+    }
+}
