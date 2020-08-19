5ed13c3859cc5bbd75680557053b3d99ae6f2cda
Fatih-Ceylan <fceylan001@gmail.com>
2020-08-19 21:47:21 +0300

    Update 20200818220529_UpdateMovieModels.cs

--- Vidly/Migrations/20200818220529_UpdateMovieModels.cs
+++ Vidly/Migrations/20200818220529_UpdateMovieModels.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 namespace Vidly.Migrations
 {
@@ -8,118 +8,118 @@ namespace Vidly.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropColumn(
-                name: "Genre",
+                name: "Genre" ,
                 table: "Movies");
 
             migrationBuilder.AlterColumn<DateTime>(
-                name: "ReleaseDate",
-                table: "Movies",
-                type: "Date",
-                nullable: true,
-                oldClrType: typeof(DateTime),
+                name: "ReleaseDate" ,
+                table: "Movies" ,
+                type: "Date" ,
+                nullable: true ,
+                oldClrType: typeof(DateTime) ,
                 oldNullable: true);
 
             migrationBuilder.AlterColumn<string>(
-                name: "Name",
-                table: "Movies",
-                maxLength: 50,
-                nullable: false,
-                oldClrType: typeof(string),
+                name: "Name" ,
+                table: "Movies" ,
+                maxLength: 50 ,
+                nullable: false ,
+                oldClrType: typeof(string) ,
                 oldNullable: true);
 
             migrationBuilder.AlterColumn<DateTime>(
-                name: "DateAdded",
-                table: "Movies",
-                type: "Date",
-                nullable: true,
-                oldClrType: typeof(DateTime),
+                name: "DateAdded" ,
+                table: "Movies" ,
+                type: "Date" ,
+                nullable: true ,
+                oldClrType: typeof(DateTime) ,
                 oldNullable: true);
 
             migrationBuilder.AddColumn<byte>(
-                name: "GenreId",
-                table: "Movies",
-                nullable: false,
-                defaultValue: (byte)0);
+                name: "GenreId" ,
+                table: "Movies" ,
+                nullable: false ,
+                defaultValue: (byte) 0);
 
             migrationBuilder.AddColumn<byte>(
-                name: "NumberInStock",
-                table: "Movies",
-                nullable: false,
-                defaultValue: (byte)0);
+                name: "NumberInStock" ,
+                table: "Movies" ,
+                nullable: false ,
+                defaultValue: (byte) 0);
 
             migrationBuilder.CreateTable(
-                name: "Genre",
+                name: "Genre" ,
                 columns: table => new
                 {
-                    Id = table.Column<byte>(nullable: false),
-                    Name = table.Column<string>(maxLength: 50, nullable: false)
-                },
+                    Id = table.Column<byte>(nullable: false) ,
+                    Name = table.Column<string>(maxLength: 50 , nullable: false)
+                } ,
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Genre", x => x.Id);
+                    table.PrimaryKey("PK_Genre" , x => x.Id);
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Movies_GenreId",
-                table: "Movies",
+                name: "IX_Movies_GenreId" ,
+                table: "Movies" ,
                 column: "GenreId");
 
             migrationBuilder.AddForeignKey(
-                name: "FK_Movies_Genre_GenreId",
-                table: "Movies",
-                column: "GenreId",
-                principalTable: "Genre",
-                principalColumn: "Id",
+                name: "FK_Movies_Genre_GenreId" ,
+                table: "Movies" ,
+                column: "GenreId" ,
+                principalTable: "Genre" ,
+                principalColumn: "Id" ,
                 onDelete: ReferentialAction.Cascade);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
-                name: "FK_Movies_Genre_GenreId",
+                name: "FK_Movies_Genre_GenreId" ,
                 table: "Movies");
 
             migrationBuilder.DropTable(
                 name: "Genre");
 
             migrationBuilder.DropIndex(
-                name: "IX_Movies_GenreId",
+                name: "IX_Movies_GenreId" ,
                 table: "Movies");
 
             migrationBuilder.DropColumn(
-                name: "GenreId",
+                name: "GenreId" ,
                 table: "Movies");
 
             migrationBuilder.DropColumn(
-                name: "NumberInStock",
+                name: "NumberInStock" ,
                 table: "Movies");
 
             migrationBuilder.AlterColumn<DateTime>(
-                name: "ReleaseDate",
-                table: "Movies",
-                nullable: true,
-                oldClrType: typeof(DateTime),
-                oldType: "Date",
+                name: "ReleaseDate" ,
+                table: "Movies" ,
+                nullable: true ,
+                oldClrType: typeof(DateTime) ,
+                oldType: "Date" ,
                 oldNullable: true);
 
             migrationBuilder.AlterColumn<string>(
-                name: "Name",
-                table: "Movies",
-                nullable: true,
-                oldClrType: typeof(string),
+                name: "Name" ,
+                table: "Movies" ,
+                nullable: true ,
+                oldClrType: typeof(string) ,
                 oldMaxLength: 50);
 
             migrationBuilder.AlterColumn<DateTime>(
-                name: "DateAdded",
-                table: "Movies",
-                nullable: true,
-                oldClrType: typeof(DateTime),
-                oldType: "Date",
+                name: "DateAdded" ,
+                table: "Movies" ,
+                nullable: true ,
+                oldClrType: typeof(DateTime) ,
+                oldType: "Date" ,
                 oldNullable: true);
 
             migrationBuilder.AddColumn<string>(
-                name: "Genre",
-                table: "Movies",
+                name: "Genre" ,
+                table: "Movies" ,
                 nullable: true);
         }
     }
